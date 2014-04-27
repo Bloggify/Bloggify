4da986416cfb853a5665d66460a6117133137b46
Ionică Bizău <bizauionica@yahoo.com>
2014-04-27 14:08:55 +0300

    Cache file contents and render them

--- apis/index.js
+++ apis/index.js
@@ -1,3 +1,62 @@
-module.exports["/api/test/"] = function (req, res) {
-    res.end(JSON.stringify({"Hello World": "From Ionică Bizău"}, null, 4));
+var Marked = require("marked")
+  , fileCache = {
+        dummyPath: {
+            ttl: setTimeout (function () {
+                delete fileCache["dummyPath"];
+            }, Config.gitSite.cache.ttl)
+          , content: "Dummy"
+        }
+    };
+
+function readFile (path, callback) {
+
+    var fromCache = fileCache[path];
+    if (fromCache) {
+
+        // update ttl
+        fromCache.ttl = setTimeout (function () {
+            console.log("Deleting " + path);
+            delete fileCache[path]
+        }, Config.gitSite.cache.ttl);
+
+        // callback content
+        return callback (null, fromCache.content);
+    }
+
+    Statique.readFile (path, function (err, content) {
+
+        if (err) {
+            return callback (err);
+        }
+
+        fileCache[path] = {
+            ttl: setTimeout (function () {
+                console.log("Deleting " + path);
+                delete fileCache[path];
+            }, Config.gitSite.cache.ttl)
+          , content: content
+        };
+
+        callback (null, content.toString());
+    });
+}
+
+module.exports["handlePage"] = function (req, res, pathName, route) {
+    readFile (route, function (err, fileContent) {
+
+        if (err) {
+            console.error (err);
+            return Statique.sendRes (res, 500, "html", "Internal server error");
+        }
+
+        Statique.sendRes (res, 200, "text/html",
+            SITE_CONFIG.parsed.roots.template.page.replace (
+                "{{PAGE_CONTENT}}"
+              , Marked (fileContent)
+            ).replace(
+                "{{TITLE}}"
+              , SITE_CONFIG.title
+            )
+        );
+    });
 };
