b1f214c1b59231a7af6ffd3c11508a780f266107
Ionică Bizău <bizauionica@yahoo.com>
2014-07-02 20:05:54 +0300

    Removed tests directory

--- tests/1.js
+++
@@ -1,9 +0,0 @@
-requireNoCache = function (path) {
-    return eval(require("fs").readFileSync("./2.js").toString())
-}
-
-console.log(requireNoCache("./2"));
-setTimeout(function () {
-    a = requireNoCache ("./2");
-    console.log(a);
-}, 4000);
