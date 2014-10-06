27898d08cc81b9f4c7f64728fc037a07ac583381
Ionică Bizău <bizauionica@yahoo.com>
2014-10-06 10:24:48 +0300

    Merge recursive function

--- utils.js
+++ utils.js
@@ -193,3 +193,19 @@ Utils.parseCookies = function (request) {
 
     return list;
 }
+
+Utils.mergeRecursive = function MergeRecursive (obj1, obj2) {
+    for (var p in obj2) {
+        try {
+            if (obj2[p].constructor == Object) {
+                obj1[p] = Utils.mergeRecursive(obj1[p], obj2[p]);
+            } else {
+                obj1[p] = obj2[p];
+            }
+        } catch (e) {
+            obj1[p] = obj2[p];
+        }
+    }
+
+    return obj1;
+};
