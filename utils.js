03559bd32c102147ada58911ef53c540c9825921
Ionică Bizău <bizauionica@yahoo.com>
2014-10-06 06:28:42 +0000

    Syntax

--- utils.js
+++ utils.js
@@ -15,8 +15,9 @@ var Utils = module.exports = require("jxutils");
 Utils.clone = function clone (item) {
     if (!item) { return item; } // null, undefined values check
 
-    var types = [ Number, String, Boolean ],
-        result;
+    var types = [ Number, String, Boolean ]
+      , result
+      ;
 
     // normalizing primitives if someone did new String('aaa'), or new Number('444');
     types.forEach(function(type) {
