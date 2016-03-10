c50fb8079004c6e9aadf6bba926883189de3d5e1
Ionică Bizău <bizauionica@gmail.com>
2016-03-10 18:15:41 +0200

    :rocket:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,11 @@
 
 const bloggifyPaths = require("../lib");
 
-console.log(bloggifyPaths());
+console.log(bloggifyPaths("~/path/to/my/app"));
+
+// BloggifyPaths {
+//   root: '/home/user/path/to/my/app',
+//   _paths: { bloggify: '.bloggify', config: 'index', plugins: 'plugins' },
+//   bloggify: '/home/user/path/to/my/app/.bloggify',
+//   config: '/home/user/path/to/my/app/.bloggify/index',
+//   plugins: '/home/user/path/to/my/app/.bloggify/plugins' }
