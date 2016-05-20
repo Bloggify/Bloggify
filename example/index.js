630b47ee37cbbbafa19b137fba00539ef17ffd30
Ionică Bizău <bizauionica@gmail.com>
2016-05-20 11:36:32 +0300

    Added tilda as dependency

--- example/index.js
+++ example/index.js
@@ -3,6 +3,7 @@
 const Bloggify = require("../lib");
 
 let app = new Bloggify(`${__dirname}/../node_modules/bloggify-app`);
+
 app.onLoad(err => {
     console.log(`Bloggify server running on port ${app._serverPort}`);
 });
