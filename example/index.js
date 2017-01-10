5da066db3669f88a2d085843f3e855c590f798a6
Ionică Bizău <bizauionica@gmail.com>
2017-01-10 11:57:44 +0200

    :memo:

--- example/index.js
+++ example/index.js
@@ -1,12 +1,29 @@
 "use strict";
 
-const bloggifyPaths = require("../lib");
+const BloggifyPaths = require("../lib");
 
-console.log(bloggifyPaths("~/path/to/my/app"));
+const Bloggify = { /* The Bloggify instance */ };
 
+// Create a new instance of BloggifyPaths
+console.log(new BloggifyPaths("~/path/to/my/app", {
+    paths: {
+        // Override the default (bloggify.js) with a JSON path.
+        config: "bloggify.json"
+    }
+}, Bloggify));
 // BloggifyPaths {
-//   root: '/home/user/path/to/my/app',
-//   _paths: { bloggify: '.bloggify', config: 'index', plugins: 'plugins' },
-//   bloggify: '/home/user/path/to/my/app/.bloggify',
-//   config: '/home/user/path/to/my/app/.bloggify/index',
-//   plugins: '/home/user/path/to/my/app/.bloggify/plugins' }
+//   root: '/home/.../path/to/my/app',
+//   _: {},
+//   _paths:
+//    { bloggify: '/',
+//      config: 'bloggify.json',
+//      plugins: '/node_modules',
+//      publicMain: '/!/bloggify/public/',
+//      publicCore: '/!/bloggify/core/',
+//      publicTheme: '/!/bloggify/theme/' },
+//   bloggify: '/home/.../path/to/my/app/',
+//   config: '/home/.../path/to/my/app/bloggify.json',
+//   plugins: '/home/.../path/to/my/app/node_modules',
+//   publicMain: '/home/.../path/to/my/app/!/bloggify/public/',
+//   publicCore: '/home/.../path/to/my/app/!/bloggify/core/',
+//   publicTheme: '/home/.../path/to/my/app/!/bloggify/theme/' }
