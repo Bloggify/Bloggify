423fc3b13a2040372c0b58a207a78e27aaa1d2b8
Ionică Bizău <bizauionica@gmail.com>
2017-08-29 12:48:23 +0300

    :boom: Semicolons

--- example/index.js
+++ example/index.js
@@ -1,11 +1,12 @@
 "use strict";
 
-const Bloggify = require("../lib");
+// This is how to include Bloggify as library.
+const Bloggify = require("../lib")
 
 // Start the Bloggify app
-const app = new Bloggify("path/to/the/application/root");
+const app = new Bloggify("path/to/the/application/root")
 
 // Do something after it's started
 app.onLoad(err => {
-    console.log(`Bloggify server running on port ${app._serverPort}`);
-});
+    console.log(`Bloggify server running on port ${app._serverPort}`)
+})
